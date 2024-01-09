581ecd3aa2158e949d4b04e89d99ea6cd0284045
munablamu <coffee.git@gmail.com>
2024-01-09 17:40:35 +0900

    3.6まで

--- components/logo.js
+++ components/logo.js
@@ -1,5 +1,10 @@
 import Link from 'next/link';
+import styles from '@/styles/logo.module.css';
 
-export default function logo() {
-  return <Link href="/">CUBE</Link>;
+export default function logo({ boxOn = false }) {
+  return (
+    <Link href="/" className={boxOn ? styles.box : styles.basic}>
+      CUBE
+    </Link>
+  );
 }
