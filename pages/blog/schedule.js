c3da49aba00e283d8d13876f81b2b1fa9e5da7f0
munablamu <coffee.git@gmail.com>
2024-01-12 14:23:41 +0900

    8.4まで

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,12 +1,37 @@
 import { getPostBySlug } from 'lib/api';
 import Container from 'components/container';
 import PostHeader from 'components/post-header';
+import PostBody from 'components/post-body';
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column';
+import Image from 'next/image';
+import ConvertBody from 'components/convert-body';
 
 export default function Schedule({ title, publish, content, eyecatch, categories }) {
   return (
     <Container>
       <article>
         <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=""
+            style={{ objectFit: 'contain', width: '100%', height: 'auto' }}
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes="{min-width: 1152px) 1152px, 100vw"
+            priority
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar></TwoColumnSidebar>
+        </TwoColumn>
       </article>
     </Container>
   );
