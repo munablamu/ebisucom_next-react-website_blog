a72dec3e54752e8ef8769e2744c01b61722d209a
munablamu <coffee.git@gmail.com>
2024-01-12 17:51:47 +0900

    9.6まで

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,13 @@
 import { getAllPosts } from 'lib/api';
+import { getImageBuffer } from 'lib/getImageBuffer';
 import Meta from 'components/meta';
 import Container from 'components/container';
 import Hero from 'components/hero';
 import Posts from 'components/posts';
+import { getPlaiceholder } from 'plaiceholder';
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants';
 
 export default function Blog({ posts }) {
   return (
@@ -18,6 +23,15 @@ export default function Blog({ posts }) {
 
 export async function getStaticProps() {
   const posts = await getAllPosts();
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal;
+    }
+    const imageBuffer = await getImageBuffer(post.eyecatch.url);
+    const { base64 } = await getPlaiceholder(imageBuffer);
+    post.eyecatch.blurDataURL = base64;
+  }
 
   return {
     props: {
