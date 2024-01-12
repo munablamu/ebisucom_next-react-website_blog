4caae10dc16067fbc7b31ea10b8654f219e369da
munablamu <coffee.git@gmail.com>
2024-01-12 16:20:49 +0900

    9.1まで

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -6,6 +6,9 @@ import PostHeader from 'components/post-header';
 import PostBody from 'components/post-body';
 import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column';
 import Image from 'next/image';
+import { getPlaiceholder } from 'plaiceholder';
+import { getImageBuffer } from 'lib/getImageBuffer';
+import { eyecatchLocal } from 'lib/constants';
 import ConvertBody from 'components/convert-body';
 import PostCategories from 'components/post-categories';
 
@@ -32,6 +35,8 @@ export default function Schedule({ title, publish, content, eyecatch, categories
             height={eyecatch.height}
             sizes="{min-width: 1152px) 1152px, 100vw"
             priority
+            placeholder="blur"
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
 
@@ -51,18 +56,24 @@ export default function Schedule({ title, publish, content, eyecatch, categories
 }
 
 export async function getStaticProps() {
-  const slug = 'schedule';
+  const slug = 'micro';
 
   const post = await getPostBySlug(slug);
 
   const description = extractText(post.content);
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal;
+
+  const imageBuffer = await getImageBuffer(eyecatch.url);
+  const { base64 } = await getPlaiceholder(imageBuffer);
+  eyecatch.blurDataURL = base64;
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description,
     },
