6178e9243ea7de3f008e97047c8db18817761277
munablamu <coffee.git@gmail.com>
2024-01-15 18:38:44 +0900

    next13 2.12まで

--- app/layout.js
+++ app/layout.js
@@ -5,16 +5,21 @@ import Layout from 'components/layout';
 
 import { siteMeta } from 'lib/constants';
 const { siteLang } = siteMeta;
+import GoogleAnalytics from 'components/googleanalytics';
 
 // Font Awesomeの設定
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
+import { Suspense } from 'react';
 config.autoAddCss = false;
 
 export default function RootLayout({ children }) {
   return (
     <html lang={siteLang}>
       <body>
+        <Suspense>
+          <GoogleAnalytics />
+        </Suspense>
         <Layout>{children}</Layout>
       </body>
     </html>
