8f54d3a4a8ecc8c5fc3c09a70d9089f0d2994ac4
munablamu <coffee.git@gmail.com>
2024-01-15 17:22:13 +0900

    next13 2.6まで

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,7 +1,7 @@
 import { getPostBySlug, getAllSlugs } from 'lib/api';
 import { extractText } from 'lib/extract-text';
 import { prevNextPost } from 'lib/prev-next-post';
-import Meta from 'components/meta';
+// import Meta from 'components/meta';
 import Container from 'components/container';
 import PostHeader from 'components/post-header';
 import PostBody from 'components/post-body';
@@ -14,25 +14,33 @@ import ConvertBody from 'components/convert-body';
 import PostCategories from 'components/post-categories';
 import Pagination from 'components/pagination';
 
-export default function Post({
-  title,
-  publish,
-  content,
-  eyecatch,
-  categories,
-  description,
-  prevPost,
-  nextPost,
-}) {
+export default async function Post({ params }) {
+  const slug = params.slug;
+
+  const post = await getPostBySlug(slug);
+
+  const { title, publishDate: publish, content, categories } = post;
+
+  const description = extractText(content);
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal;
+
+  const imageBuffer = await getImageBuffer(eyecatch.url);
+  const { base64 } = await getPlaiceholder(imageBuffer);
+  eyecatch.blurDataURL = base64;
+
+  const allSlugs = await getAllSlugs();
+  const [prevPost, nextPost] = prevNextPost(allSlugs, slug);
+
   return (
     <Container>
-      <Meta
+      {/* <Meta
         pageTitle={title}
         pageDesc={description}
         pageImg={eyecatch.url}
         pageImgW={eyecatch.width}
         pageImgH={eyecatch.height}
-      />
+  /> */}
 
       <article>
         <PostHeader title={title} subtitle="Blog Article" publish={publish} />
@@ -74,41 +82,11 @@ export default function Post({
   );
 }
 
-export async function getStaticPaths() {
+export const dynamicParams = false;
+export async function generateStaticParams() {
   const allSlugs = await getAllSlugs();
 
-  return {
-    paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
-    fallback: false,
-  };
-}
-
-export async function getStaticProps(context) {
-  const slug = context.params.slug;
-
-  const post = await getPostBySlug(slug);
-
-  const description = extractText(post.content);
-
-  const eyecatch = post.eyecatch ?? eyecatchLocal;
-
-  const imageBuffer = await getImageBuffer(eyecatch.url);
-  const { base64 } = await getPlaiceholder(imageBuffer);
-  eyecatch.blurDataURL = base64;
-
-  const allSlugs = await getAllSlugs();
-  const [prevPost, nextPost] = prevNextPost(allSlugs, slug);
-
-  return {
-    props: {
-      title: post.title,
-      publish: post.publishDate,
-      content: post.content,
-      eyecatch: eyecatch,
-      categories: post.categories,
-      description: description,
-      prevPost: prevPost,
-      nextPost: nextPost,
-    },
-  };
+  return allSlugs.map(({ slug }) => {
+    return { slug: slug };
+  });
 }
